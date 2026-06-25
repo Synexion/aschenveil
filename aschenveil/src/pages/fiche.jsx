8e5f076651f1f08f05fb7052abfe4f1dda162f1f
Synexion <liliansynexion@gmail.com>
2026-06-25 22:39:10 +0200

    feat: Ma fiche reliée au token JWT

--- aschenveil/src/pages/fiche.jsx
+++ aschenveil/src/pages/fiche.jsx
@@ -1,14 +1,12 @@
 import {useEffect} from 'react';
 import { useNavigate } from 'react-router-dom';
-
-const profil = {
-  pseudo:"test", dateInscription:"24/06/2026", topic: [{id:1, title:"test", date:"24/06/2026"}], com: [{id:1, title:"test", date:"24/06/2026"}, {id:2, title:"test 2", date:"20/06/2026"}]
-};
+import { jwtDecode } from 'jwt-decode';
 
 function Fiche (){
 
   const navigate = useNavigate();
   const token = localStorage.getItem('token');
+  const decoded = token ? jwtDecode(token) : null;
 
   useEffect(() => {
     if(!token) {
@@ -25,23 +23,23 @@ function Fiche (){
       <div className="flex items-center text-center gap-5 justify-around mt-5 bg-black/70 p-5">
         <div>
           <img src="/hero.png" alt="avatar" className="h-20 w-20" />
-          <h3>{profil.pseudo}</h3>
+          <h3>{decoded.pseudo}</h3>
         </div>
         <div>
-          <p>{profil.dateInscription}</p>
+          <p>date inscription</p>
         </div>
       </div>
       <div className="flex mt-5 text-center bg-black/60">
         <h2 className="uppercase font-bold">topic créer</h2>
       </div>
       <div className="flex mt-5 text-center bg-black/60">
-        {profil.topic.map(t => (<p key={t.id}>{t.title} - {t.date}</p>))}
+        <p>bientot disponible</p>
       </div>
       <div className="flex mt-5 text-center bg-black/60">
         <h2 className="uppercase font-bold">Commentaire créer</h2>
       </div>
       <div className="flex mt-5  flex-col gap-5">
-        {profil.com.map(c => (<p key={c.id} className="bg-black/60">{c.title} - {c.date}</p>))}
+        <p>bientot disponible</p>
       </div>
     </div>
   )
